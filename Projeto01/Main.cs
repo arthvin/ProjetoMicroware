ee24666b4aa0631c567aa4f351963b31e3492328
Arthur Semensati <112193063+arthvin@users.noreply.github.com>
2024-08-09 17:37:11 -0300

    Update Main.cs

--- Projeto01/Main.cs
+++ Projeto01/Main.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +15,18 @@ namespace Projeto01
     {
         private int _power = 10;
         private Microwave microwave;
+        private List<Program> _customPrograms;
+
+        public class Program
+        {
+            public string Nome { get; set; }
+            public string Alimento { get; set; }
+            public int Potencia { get; set; }
+            public int Tempo { get; set; }
+            public string Caractere { get; set; }
+            public string Instrucoes { get; set; }
+        }
+
         public Main()
         {
             microwave = new Microwave();
@@ -40,7 +52,7 @@ namespace Projeto01
         }
         private void btnPotencia_Click(object sender, EventArgs e)
         {
-            if (Int32.Parse(edtMinutos.Text) > 0 || Int32.Parse(edtMinutos.Text) <= 10 )
+            if (Int32.Parse(edtMinutos.Text) > 0 || Int32.Parse(edtMinutos.Text) <= 10)
             {
                 _power = Int32.Parse(edtMinutos.Text);
             }
@@ -54,7 +66,7 @@ namespace Projeto01
 
         private void btn1_Click(object sender, EventArgs e)
         {
-            edtMinutos.Text = (edtMinutos.Text + '1');     
+            edtMinutos.Text = (edtMinutos.Text + '1');
         }
 
         private void btn2_Click(object sender, EventArgs e)
@@ -94,7 +106,7 @@ namespace Projeto01
 
         private void btn9_Click(object sender, EventArgs e)
         {
-           edtMinutos.Text = (edtMinutos.Text + '9');
+            edtMinutos.Text = (edtMinutos.Text + '9');
         }
 
         private void btn0_Click(object sender, EventArgs e)
@@ -130,6 +142,44 @@ namespace Projeto01
         private void button14_Click(object sender, EventArgs e)
         {
             microwave.StartPredefinedProgram("Carnes de Boi", lblMicroondas);
+        }
+        private void btnAdicionarPrograma_Click(object sender, EventArgs e)
+        {
+            string nome = txtNomePrograma.Text;
+            string alimento = txtAlimentoPrograma.Text;
+            int potencia;
+            int tempo;
+            string caractere = txtCaracterePrograma.Text;
+
+            if (string.IsNullOrWhiteSpace(nome) ||
+                string.IsNullOrWhiteSpace(alimento) ||
+                !int.TryParse(txtPotenciaPrograma.Text, out potencia) ||
+                !int.TryParse(txtTempoPrograma.Text, out tempo) ||
+                string.IsNullOrWhiteSpace(caractere) ||
+                caractere == ".")
+            {
+             
+                return;
+            }
+
+            Program novoPrograma = new Program
+            {
+                Nome = nome,
+                Alimento = alimento,
+                Potencia = potencia,
+                Tempo = tempo,
+                Caractere = caractere,
+            };
+
+            _customPrograms.Add(novoPrograma);
+        }
+
+        private void txtCaracterePrograma_DragLeave(object sender, EventArgs e)
+        {
+            if (txtCaracterePrograma.TextLength > 1)
+            {
+                txtCaracterePrograma.Clear();
+            }
         }
     }
 }
